525a2d3c6d3e79ddd4118cba2aae75903fad77f9
ali sajjadi <sajjadia997@gmail.com>
2025-07-24 13:03:30 +0330

    our markdown test has added

--- tuple/tupplesession.py
+++ tuple/tupplesession.py
@@ -3,6 +3,9 @@
 # l = [i ** 2 for i in range(1,11)]
 # print(l)
 
+#========================================================
+
+
 # tuple
 # tup = (1,"hello",True,"salam",'reza','mahsa')
 # print(type(tup))
