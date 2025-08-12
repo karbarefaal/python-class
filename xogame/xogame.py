08dc0c9e8303a3a987a493bf07d3a9a33443b7a0
Ali Sajjadi <sajjadia997@gmail.com>
2025-08-12 03:42:36 +0330

    we can move in matrix using arrow keys

--- xogame/xogame.py
+++ xogame/xogame.py
@@ -1,35 +1,69 @@
 import keyboard
+import os
+import time
 
-ground = [['?','?','?'],['?','?','?'],['?','?','?']]
-len_ground_x = len(ground)
-# cursor_x,cursur_y = 0,0
-# len_ground_y = len(ground[0])
-# print(len_ground_x,len_ground_y)
+def ground_maker(n):
+    ground = []
+    for i in range(n):
+        ground.append([])
+        for j in range(n):
+            ground[i].append(' ')
+    return ground
+
+ground = ground_maker(3)
+len_ground = len(ground)
+cursor_x,cursor_y = 0,0
 
 def play_ground():
-    for i in range(0,len_ground_x):
-        for x in range(0,len_ground_x):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    for i in range(len_ground):
+        for x in range(len_ground):
             print(' ---',end='')
         print()
         print('|',end=' ')
-        for j in range(0,len_ground_x):
-            # if i == cursur_y and j == cursor_x:
-            print(ground[i][j],end=' ')
-            print('|',end= ' ')
+        for j in range(len_ground):
+            if i == cursor_y and j == cursor_x:
+                ground[i][j] = '?'
+                print(ground[i][j],'|',end= ' ')
+            else:
+                ground[i][j] = ' '
+                print(ground[i][j],end=' ')
+                print('|',end= ' ')
         print()
-    for x in range(0,len_ground_x):
+    for x in range(len_ground):
         print(' ---',end='')
     print()
 
 play_ground()
 
 while True:
-    event = keyboard.read_event()
-    if event.event_type == keyboard.KEY_DOWN:
-        if event.name == 'esc':
-            print('Done!')
-            exit()
-        elif event.name == 'left':
-            pass
+    moved = False
+    if keyboard.is_pressed('left'):
+        if cursor_x > 0:
+            cursor_x -= 1
+            moved = True
+    elif keyboard.is_pressed('right'):
+        if cursor_x < len_ground - 1:
+            cursor_x += 1
+            moved = True
+    elif keyboard.is_pressed('up'):
+        if cursor_y > 0:
+            cursor_y -= 1
+            moved = True
+    elif keyboard.is_pressed('down'):
+        if cursor_y < len_ground - 1:
+            cursor_y += 1
+            moved = True
+    if moved:
+        play_ground()
+        time.sleep(0.3)  # debounce
 
+
+
+
+
+    # if event.event_type == keyboard.KEY_DOWN:
+    #     if event.name == 'esc':
+    #         print('Done!')
+    #         exit()
 
