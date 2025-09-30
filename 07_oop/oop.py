75abca1d416800abe7d6fba56a93c4f6d1989ac5
Ali Sajjadi <sajjadia997@gmail.com>
2025-09-30 10:49:15 +0330

    oop third session

--- 07_oop/oop.py
+++ 07_oop/oop.py
@@ -1,3 +1,54 @@
+#===========================third session===================
+
+class Book:
+    """
+    This class is about Book
+    """
+    def __init__ (self, title, author, published_at):
+        self.title = title
+        self.author = author
+        self.published_at = published_at
+
+    def infoBook(self):
+        show = f"""the book name is {self.title} and the author of the book
+is {self.author} and also this book was published at {self.published_at}"""
+        print(show)
+
+    def __str__(self):
+        return f"""
+Book title: {self.title} - 
+Author: {self.author} - 
+Published at: {self.published_at}
+"""
+    
+    def book_list(my_list: list):
+        strList = []
+        for i in my_list:
+            strList.append(str(i))
+        return strList
+        # return [str(book) for book in my_list]    # second way
+
+   
+
+b1 = Book("Harry potter", "J. K. Rowling", 1997)
+b2 = Book("shahnameh", "ferdosi", 400)
+b3 = Book("leili va majnoon", "nezami", 500)
+# print(b1.title)
+# print(b1.author)
+# print(b1.published_at)
+# print(b2.title)
+# print(b2.author)
+# print(b2.published_at)
+# print(b3.title)
+# print(b3.author)
+# print(b3.published_at)
+
+
+# b1.infoBook()
+bookList = [b1,b2,b3]
+
+print(Book.book_list(bookList))
+
 #============================second session================================
 
 class MyNewClass():
