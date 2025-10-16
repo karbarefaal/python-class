9b68f112c6ab1b5cc5cf36fecd40aed6aa88cf5c
ali sajjadi <sajjadia997@gmail.com>
2025-10-16 18:28:48 +0330

    inheritance2

---
+++ exercises/oop_example/teacher.py
@@ -0,0 +1,21 @@
+from course import Course
+from student import Student
+
+class Teacher:
+    def __init__(self, name, madrak, exp):
+        self.name = name
+        self.madrak = madrak
+        self.exp = exp
+
+    def add_course(self, course_list: list, course: Course):
+        course_list.append(course)
+        return  course_list
+    def student_list(self):
+        return student_list
+    
+    
+if "__name__" == "__main__":
+    student_list = {
+        "mamad": "riazi"
+    }
+    
